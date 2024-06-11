e15de806df93d7684d63d6c8feb045c59d6eeea9
Максим Шостак <Shostak.M@digdes.com>
2024-06-11 17:43:04 +0300

    https://learn.microsoft.com/ru-ru/aspnet/core/tutorials/first-mvc-app/controller-methods-views?view=aspnetcore-8.0

---
+++ WebApp/Data/WebAppContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using WebApp.Models;
+using System.Reflection;
+
+namespace WebApp.Data
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime? SetKindUtc(this DateTime? dateTime)
+        {
+            if (dateTime.HasValue)
+            {
+                return dateTime.Value.SetKindUtc();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static DateTime SetKindUtc(this DateTime dateTime)
+        {
+            if (dateTime.Kind == DateTimeKind.Utc) { return dateTime; }
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        }
+    }
+
+    public static class UtcDateAnnotation
+    {
+        private const string IsUtcAnnotation = "IsUtc";
+        private static readonly ValueConverter<DateTime, DateTime> UtcConverter = new ValueConverter<DateTime, DateTime>(convertTo => DateTime.SpecifyKind(convertTo, DateTimeKind.Utc), convertFrom => convertFrom);
+
+        public static PropertyBuilder<TProperty> IsUtc<TProperty>(this PropertyBuilder<TProperty> builder, bool isUtc = true) => builder.HasAnnotation(IsUtcAnnotation, isUtc);
+
+        public static bool IsUtc(this IMutableProperty property)
+        {
+            if (property != null && property.PropertyInfo != null)
+            {
+                var attribute = property.PropertyInfo.GetCustomAttribute<IsUtcAttribute>();
+                if (attribute is not null && attribute.IsUtc)
+                {
+                    return true;
+                }
+
+                return ((bool?)property.FindAnnotation(IsUtcAnnotation)?.Value) ?? true;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Make sure this is called after configuring all your entities.
+        /// </summary>
+        public static void ApplyUtcDateTimeConverter(this ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (!property.IsUtc())
+                    {
+                        continue;
+                    }
+
+                    if (property.ClrType == typeof(DateTime) ||
+                        property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetValueConverter(UtcConverter);
+                    }
+                }
+            }
+        }
+    }
+    public class IsUtcAttribute : Attribute
+    {
+        public IsUtcAttribute(bool isUtc = true) => this.IsUtc = isUtc;
+        public bool IsUtc { get; }
+    }
+
+    public class WebAppContext : DbContext
+    {
+        public WebAppContext (DbContextOptions<WebAppContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyUtcDateTimeConverter();//Put before seed data and after model creation
+        }
+        public DbSet<WebApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
